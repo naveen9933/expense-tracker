b6139f8c254b01b6e0f62fd311b4bcc0d87bc3d1
naveen9933 <gnc9933@gmail.com>
2024-04-17 13:42:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,38 +1,61 @@
 import { useState } from "react";
-import ExpenseItem from "./components/Expenses/Expenses"
-import './App.css';
+import ExpenseItem from "./components/Expenses/Expenses";
+import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
 const DUMMY_EXPENSES = [
   {
-    id: 'e1',
-    title: 'Toilet Paper',
+    id: "e1",
+    title: "Toilet Paper",
     amount: 94.12,
-    date: new Date(2020, 7, 14)
+    date: new Date(2020, 7, 14),
   },
   {
-    id: 'e2',
-    title: 'New TV',
+    id: "e2",
+    title: "New TV",
     amount: 799.49,
-    date: new Date(2021, 2, 12)
+    date: new Date(2021, 2, 12),
   },
   {
-    id: 'e3',
-    title: 'Car Insurance',
+    id: "e3",
+    title: "Car Insurance",
     amount: 294.67,
-    date: new Date(2020, 2, 28)
+    date: new Date(2020, 2, 28),
   },
   {
-    id: 'e4',
-    title: 'New Desk (Wodden)',
+    id: "e4",
+    title: "New Desk (Wodden)",
     amount: 450,
-    date: new Date(2021, 5, 12)
+    date: new Date(2021, 5, 12),
   },
-]
+  {
+    id: "e5",
+    title: "Trip",
+    amount: 940,
+    date: new Date(2024, 1, 14),
+  },
+  {
+    id: "e6",
+    title: "New Phone",
+    amount: 12000,
+    date: new Date(2024, 2, 12),
+  },
+  {
+    id: "e7",
+    title: "Car Insurance",
+    amount: 2900,
+    date: new Date(2024, 2, 28),
+  },
+  {
+    id: "e8",
+    title: "New Desk (Wodden)",
+    amount: 4500,
+    date: new Date(2024, 3, 12),
+  },
+];
 
 const App = () => {
-
   const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
@@ -46,6 +69,6 @@ const App = () => {
       <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
